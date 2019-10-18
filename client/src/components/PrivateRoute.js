acf71f3a1e947ffb5cbcd6fa7d801c2981bd4d49
theJOHNwatt <j.watt10@gmail.com>
2019-10-18 10:20:15 -0700

    pages redirected

--- client/src/components/PrivateRoute.js
+++ client/src/components/PrivateRoute.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 
-const PrivateRoute = ({component: CompositionEvent, ...rest}) => {
+const PrivateRoute = ({component: Component, ...rest}) => {
     return (
         <Route
         {...rest}
         render={props => {
             if(localStorage.getItem('token')) {
-                return <Compoenent {...props} />;
+                return <Component {...props} />;
             }else {
                 return <Redirect to='/login/' />
             }
--- client/src/utils/axiosWithAuth.js
+++ client/src/utils/axiosWithAuth.js
@@ -5,7 +5,7 @@ export const axiosWithAuth = () => {
     return axios.create({
         baseURL: 'http://localhost:5000',
         headers: {
-            Authorization: token
+            authorization: token
         }
     });
-};
+}
